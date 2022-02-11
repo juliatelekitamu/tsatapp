95fe815e0118dd6d8f6d9c5fdb021abf9543be60
Julia Teleki <juliateleki@tamu.edu>
2022-02-10 18:21:57 -0600

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # Add route for root
+  root "main#index"
+
+  resources :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
